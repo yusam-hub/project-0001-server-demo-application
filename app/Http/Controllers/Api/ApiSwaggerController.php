94cbc343b3671551aabd51fd247ef7db4ad90b49
yusam <yusam@yandex.ru>
2023-08-06 02:30:57 +0300

    #changed

--- app/Http/Controllers/Api/ApiSwaggerController.php
+++ app/Http/Controllers/Api/ApiSwaggerController.php
@@ -6,19 +6,21 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ApiSwaggerController extends \YusamHub\AppExt\SymfonyExt\Http\Controllers\ApiSwaggerController
 {
+    const MODULE_V1 = 'v1';
+
+    const MODULES = [
+        self::MODULE_V1,
+    ];
+
     protected static function getSwaggerModules(): array
     {
-        return [
-            'debug',
-            'v1',
-         ];
+        return self::MODULES;
     }
 
     protected function getOpenApiScanPaths(Request $request, string $module): array
     {
         return [
-            __DIR__ . DIRECTORY_SEPARATOR . ucfirst($module),
-            app_ext()->getRootDir('/vendor/yusam-hub/app-ext/src/SymfonyExt/Http/Controllers/Api/' . ucfirst($module))
+            __DIR__ . DIRECTORY_SEPARATOR . ucfirst($module)
         ];
     }
 }
--- app/Http/Controllers/Api/V1/V1ControllerApi.php
+++ app/Http/Controllers/Api/V1/V1ControllerApi.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Api\ApiSwaggerController;
 use App\Http\Controllers\Api\BaseApiHttpController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class V1ControllerApi extends BaseApiHttpController
 {
+    const MODULE_CURRENT = ApiSwaggerController::MODULE_V1;
     public static function routesRegister(RoutingConfigurator $routes): void
     {
-        static::routesAdd($routes, ['OPTIONS', 'GET'],'/api/v1', 'getApiHome');
+        static::routesAdd($routes, ['OPTIONS', 'GET'],sprintf('/api/%s', self::MODULE_CURRENT), 'getApiHome');
     }
 
     /**

69127d27652ae329e125b21113ff18225aa452f8
yusam <yusam@yandex.ru>
2023-10-29 00:22:02 +0300

    #changed

--- config/api-clients.php
+++ config/api-clients.php
@@ -3,7 +3,7 @@
 return [
     \App\ApiClients\ClientAuthAppSdk::class => [
         'isDebugging' => true,
-        'baseUrl' => 'https://esa.mayapur.pp.ru.loc',
+        'baseUrl' => 'https://auth.mayapur.pp.ru.loc',
         'storageLogFile' => app_ext()->getStorageDir('/logs/') . 'curl-ext-debug-app-sdk.log',
         "identifierId" => app_ext_env("CLIENT_AUTH_APP_SDK_APP_ID", 0),
         "privateKey" =>  app_ext_env("CLIENT_AUTH_APP_SDK_APP_PRIVATE_KEY", '')
